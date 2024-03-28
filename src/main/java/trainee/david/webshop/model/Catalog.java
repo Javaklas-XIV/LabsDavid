019f8098b4ff2cbf1ac15f717412be605f71ba4d
DavidKoens <david.koens@novi-education.nl>
2024-03-28 11:10:30 +0100

    Made Catalog iterable

--- src/main/java/trainee/david/webshop/model/Catalog.java
+++ src/main/java/trainee/david/webshop/model/Catalog.java
@@ -1,9 +1,10 @@
 package trainee.david.webshop.model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class Catalog {
+public class Catalog implements Iterable<Item> {
 
     private final List<Item> catalogItems;
     private final int year;
@@ -11,6 +12,23 @@ public class Catalog {
     public Catalog(int year){
         this.year = year;
         catalogItems = new ArrayList<>();
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new Iterator<>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < catalogItems.size();
+            }
+
+            @Override
+            public Item next() {
+                return catalogItems.get(index++);
+            }
+        };
     }
 
     public int getYear() {
